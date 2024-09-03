89f94e413bad2189e1392a7f27cf3c460db0caf5
BuildTools <unconfigured@null.spigotmc.org>
2024-09-03 11:46:11 +0300

    added base model

---
+++ OrderTrackingItemCatalogService.Models/ItemCatalogModels/Item.cs
@@ -0,0 +1,17 @@
+﻿using OrderTrackingItemCatalogService.Models.ItemCatalogModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderTrackingItemCatalogService.Models.ItemCatalogModels
+{
+    public class Item : Entity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int StockQuantity { get; set; }
+    }
+}
---
+++ OrderTrackingItemCatalogService/Data/ItemCatalogContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OrderTrackingItemCatalogService.Data
+{
+    public class ItemCatalogContext : DbContext
+    {
+        public ItemCatalogContext(DbContextOptions<ItemCatalogContext> options) : base(options) { }
+
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
