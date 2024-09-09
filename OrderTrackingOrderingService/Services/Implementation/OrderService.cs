3dc960250a519d4941a2962336392ac78acf006b
MrArthur0507 <mrarthur0507@gmail.com>
2024-09-09 11:57:49 +0300

    Added Duende Identity Server

---
+++ OrderTrackingOrderingService/Services/Implementation/OrderService.cs
@@ -0,0 +1,11 @@
+﻿using OrderTrackingOrderingService.DataAccess.Context;
+
+namespace OrderTrackingOrderingService.Services.Implementation
+{
+    public class OrderService
+    {
+
+        private readonly OrderingDbContext _dbContext;
+        public OrderService() { }
+    }
+}
