d17ec9f7099701edf8e86894bf1681e9f9e6df05
MrArthur0507 <mrarthur0507@gmail.com>
2024-09-10 10:45:33 +0300

    added testing client for identity server and register razor page

--- OrderTrackingIdentityServer/Config.cs
+++ OrderTrackingIdentityServer/Config.cs
@@ -21,32 +21,15 @@ public static class Config
     public static IEnumerable<Client> Clients =>
         new Client[]
         {
-            // m2m client credentials flow client
+            //only for development and testing purposes
             new Client
             {
-                ClientId = "m2m.client",
-                ClientName = "Client Credentials Client",
-
-                AllowedGrantTypes = GrantTypes.ClientCredentials,
-                ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
-
-                AllowedScopes = { "scope1" }
-            },
-
-            // interactive client using code flow + pkce
-            new Client
-            {
-                ClientId = "interactive",
-                ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
-
-                AllowedGrantTypes = GrantTypes.Code,
-
-                RedirectUris = { "https://localhost:44300/signin-oidc" },
-                FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
-                PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
-
-                AllowOfflineAccess = true,
-                AllowedScopes = { "openid", "profile", "scope2" }
-            },
+                ClientId = "postman",
+                ClientName = "Postman",
+                AllowedScopes = {"openid", "profile"},
+                RedirectUris = {"https://www.getpostman.com/oauth2/callback"},
+                ClientSecrets = new[] {new Secret("testSecret".Sha256())},
+                AllowedGrantTypes = {GrantType.ResourceOwnerPassword}
+            }
         };
 }
