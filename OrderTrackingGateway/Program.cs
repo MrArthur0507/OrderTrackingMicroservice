e35497599b16bab631c97fb44c95fb9b05559544
MrArthur0507 <mrarthur0507@gmail.com>
2024-09-16 15:34:02 +0300

    configured basic routes and cluster for existing microservices

--- OrderTrackingGateway/Program.cs
+++ OrderTrackingGateway/Program.cs
@@ -1,25 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+    endpoints.MapReverseProxy();
+});
 
 app.Run();
