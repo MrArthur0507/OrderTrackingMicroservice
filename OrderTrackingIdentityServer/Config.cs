fa2203aa1462378e960da983b0fd8addf97f5fb7
MrArthur0507 <mrarthur0507@gmail.com>
2024-09-16 15:21:14 +0300

    added new project for gateway using yarp

--- OrderTrackingIdentityServer/Config.cs
+++ OrderTrackingIdentityServer/Config.cs
@@ -29,17 +29,21 @@ public static class Config
                 AllowedScopes = {"openid", "profile"},
                 RedirectUris = {"https://www.getpostman.com/oauth2/callback"},
                 ClientSecrets = new[] {new Secret("testSecret".Sha256())},
-                AllowedGrantTypes = {GrantType.ResourceOwnerPassword}
+                AllowedGrantTypes = {GrantType.ResourceOwnerPassword},
+
             },
             new Client
             {
                 ClientId = "angular",
                 ClientName = "Angular",
                 AllowedScopes = {"openid", "profile"},
-                RedirectUris = {"https://localhost:4200/signin-oidc"},
-                ClientSecrets = new[] {new Secret("testSecret".Sha256())},
+                RedirectUris = {"http://localhost:4200"},
+                PostLogoutRedirectUris = { "http://localhost:4200" },
                 AllowedGrantTypes = {GrantType.AuthorizationCode},
-                
+                RequirePkce = true,
+                RequireClientSecret = false,
+                AllowedCorsOrigins = { "http://localhost:4200" },
+                AllowAccessTokensViaBrowser = true,
             }
         };
 }
